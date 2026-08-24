2402c0e7d32fc0b95cbc7ad1d704514bff02c21a
aashutoshk2003 <aashutoshk2003@gmail.com>
2026-08-24 12:01:10 +0530

    solve: Updated 189 solution

--- 01-Arrays/189-Rotate-Array/Solution.java
+++ 01-Arrays/189-Rotate-Array/Solution.java
@@ -3,19 +3,19 @@ class Solution {
         int n = nums.length;
         k = k % n;
 
-        reverse(nums, n - 1, n - k);
-        reverse(nums, n - k - 1, 0);
-        reverse(nums, n - 1, 0);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, 0, n - 1);
     }
 
-    public static void reverse(int[] nums, int start, int end) {
-        while (start > end) {
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
 
-            start--;
-            end++;
+            start++;
+            end--;
         }
     }
 }
